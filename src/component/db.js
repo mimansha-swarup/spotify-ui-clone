7f50d39df0d73083d0ccf794fb701d7ee7c4ef22
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-05-10 18:47:14 +0530

    Json Database

---
+++ src/component/db.js
@@ -0,0 +1,193 @@
+export const Songs = [{
+    title: "Recently Played",
+    list: [{
+            song: "Rang Lageya",
+            singer: "Mohit Chauhan",
+            duration: "3:47",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/Recently%20Played/RangLageya.jpg"
+        }, {
+            song: "Raba Mehar Kari",
+            singer: "Darshan Raval",
+            duration: "3:28",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/Recently%20Played/RabaMeharKari.jpg"
+        },
+        {
+            song: "Filhal",
+            singer: "B Praak",
+            duration: "4:15",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/Recently%20Played/FIlhal.jpg"
+        },
+        {
+            song: "Raabta",
+            singer: "Pritam, Arijit Singh",
+            duration: "4:03",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/Recently%20Played/Raaabta.jpg"
+        },
+        {
+            song: "Teri Miti",
+            singer: "Arko, B Praak",
+            duration: "5:13",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/Recently%20Played/veMahi.jpg"
+        }
+    ]
+}, {
+    title: "International Hits",
+    list: [{
+            song: "100 Ways",
+            singer: "Jackson Wang",
+            duration: "2:48",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/International%20hit/100ways.jpg"
+        }, {
+            song: "Before You GO",
+            singer: "Lewis Capaldi",
+            duration: "3:35",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/International%20hit/BeforeYouGo.jpg"
+        },
+        {
+            song: "Changes",
+            singer: "XXXTENTAICON",
+            duration: "2:01",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/International%20hit/Changes.jpg"
+        },
+        {
+            song: "Higher Power",
+            singer: "Coldplay",
+            duration: "2:44",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/International%20hit/HighPower.jpg"
+        },
+        {
+            song: "Unstoppable",
+            singer: "Dino James",
+            duration: "4:39",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/International%20hit/Unstoppable.jpg"
+        }
+    ]
+}, {
+    title: "More From Darshan Raval",
+    list: [{
+            song: "Barish",
+            singer: "Darshan Raval",
+            duration: "2:51",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone//main/img_db/More%20From%20Darshan%20Raval/Abphirsejbbaarishhogi.jpg"
+        }, {
+            song: "Bhul Dunga",
+            singer: "Darshan Raval",
+            duration: "3:06",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/More%20From%20Darshan%20Raval/Bhuladunga.jpg"
+        },
+        {
+            song: "Hawa Banke",
+            singer: "Darshan Raval",
+            duration: "3:50",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/More%20From%20Darshan%20Raval/HawaBanke.jpg"
+        },
+        {
+            song: "Kaash Aisa Hota",
+            singer: "Darshan Raval",
+            duration: "3:02",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/More%20From%20Darshan%20Raval/KaashAisaHota.jpg"
+        },
+        {
+            song: "Ek Tarfa",
+            singer: "Darshan Raval",
+            duration: "3:54",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/More%20From%20Darshan%20Raval/ek-tarfa.jpg"
+        }
+    ]
+}, {
+    title: "You Might Like",
+    list: [{
+            song: "Agar Tum Saath Ho",
+            singer: "Alka Yagnik, Arijit Singh",
+            duration: "5:41",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone//main/img_db/you%20might%20like/AgarTumSaat.jpg"
+        }, {
+            song: "Guzaarish",
+            singer: "Javed Ali",
+            duration: "5:27",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/Guzarish.jpg"
+        },
+        {
+            song: "Main Agar Kahoon",
+            singer: "Sonu Niggam",
+            duration: "5:08",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/Main%20agar%20kahoon.jpg"
+        },
+        {
+            song: "Kaise Hua",
+            singer: "Vishal Mishra",
+            duration: "3:54",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/kaise%20hua.jpg"
+        },
+        {
+            song: "Main Bola Hey",
+            singer: "Karthik Rao",
+            duration: "2:33",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/wo%20boli%20hey.jpg"
+        }
+    ]
+}, {
+    title: "Podcast",
+    list: [{
+            song: "Bill Gates",
+            singer: "",
+            duration: "",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone//main/img_db/podcast/bill.jpg"
+        }, {
+            song: "Elon Musk",
+            singer: "",
+            duration: "",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/podcast/elon.jpg"
+        },
+        {
+            song: "Mark Zuckerberg",
+            singer: "",
+            duration: "",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/podcast/markzuckerbug.jpg"
+        },
+        {
+            song: "Sachin Tebdulkar",
+            singer: "",
+            duration: "",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/podcast/sachinTendulkar.jpg"
+        },
+        {
+            song: "Steve Jobs",
+            singer: "",
+            duration: "",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/podcast/steveJobs.jpg"
+        }
+    ]
+}, {
+    title: "90s Hit",
+    list: [{
+            song: "Pehla Nasha",
+            singer: "Udit Narayan",
+            duration: "4:51",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone//main/img_db/90s%20hit/pehlanasha.jpg"
+        }, {
+            song: "Dil Chata Hai",
+            singer: "Shankar-Eshan-Loy",
+            duration: "3:06",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/90s%20hit/wohldkihaikahan.jpg"
+        },
+        {
+            song: "Mohabatein",
+            singer: "Grisel Chinchay",
+            duration: "4:50",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/90s%20hit/HumkoHUmiSeChuraLO.jpg"
+        },
+        {
+            song: "Tujhe Dekha Toh",
+            singer: "Lata Mangeshkar",
+            duration: "5:02",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/90s%20hit/tujheDekhaTOh.jpg"
+        },
+        {
+            song: "Tadap Tadap",
+            singer: "KK,Dominique",
+            duration: "6:37",
+            image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/90s%20hit/chaandchupa.jpg"
+        }
+    ]
+}]
