ac1731da8194925d2e7e103d07b24e8fab978c90
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-05-10 18:48:18 +0530

    minor Refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import Home from "./component/Home";
 import SideBar from "./component/SideBar";
+import { IoChevronBackCircleOutline ,IoChevronForwardCircleOutline } from 'react-icons/io5'
+import { IconContext  } from 'react-icons'
 
 
 
 function App() {
     return ( 
+      <IconContext.Provider value={{ className: 'tile-logo'}}>
       <div className="row">
         <SideBar />
-        <div className="content">
-        <Home />
-        <Home />
-        <Home />
-
-        
-        
+        <div className="column">
+          <div className="nav-details">
+            <div>
+            <IoChevronBackCircleOutline />
+            <IoChevronForwardCircleOutline />
+            </div>
+            
+            <button className="upgrade-btn">
+            UPGRADE
+            </button>
+            
           
+          </div>
+          
+          
+          <div className="content">
+          <Home />
+          <div style={{height:'20vh'}}></div>
+         
+          
+          
+          
+          
+          </div>
         </div>
       </div>
-    );
-}
+      </IconContext.Provider>
+      );
+    }
 
 export default App;
