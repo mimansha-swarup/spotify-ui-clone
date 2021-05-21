3220add6a605c44795ba24a6e07d0f3623a7a5f5
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-05-21 10:23:31 +0530

    refactor: new Routes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from "./component/Home";
 import NavBar from "./component/commonComponent/NabBar";
 import PlayBar from "./component/commonComponent/PlayBar";
-
 import SideBar from "./component/commonComponent/SideBar";
-import LikedSongs from "./component/LikePage";
 import SearchPg from './component/searchpg';
-// import { useEffect } from 'react';
 import ScrollToTop from './component/commonComponent/ScrollToTop';
+import Library from './component/Library';
+import LikedSongs from './component/Liked';
 
 
 
 
 function App() {
-    // useEffect(() => {
-    //     window.scrollTo(0, 0)
-    //   })
     return ( 
         <Router >
         <ScrollToTop />
@@ -28,7 +24,8 @@ function App() {
                 <Switch>
                     <Route exact path='/' component={Home}/>
                     <Route path='/search' component={SearchPg} />
-                    <Route path='/collection/tracks' component={LikedSongs} />
+                    <Route exact path='/collection' component={Library} />
+                    <Route exact path='/collection/tracks' component={LikedSongs} />
                 
                 </Switch>
                 <PlayBar />
