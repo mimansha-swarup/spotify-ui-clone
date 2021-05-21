fb616e21bf55fcb8c3c572ac8c11efd59eb819dc
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-05-21 10:42:47 +0530

    feat: Like Page

--- src/component/Liked.js
+++ src/component/Liked.js
@@ -1,12 +1,12 @@
 import "./Liked.css"
 import DetailHead from "./detailsPageComponent/HeadBox"
 import SongListing from "./detailsPageComponent/SongListing"
-import {fav} from "../db/Library.js"
+import {heartSOngs} from "../db/Librarydb.js"
 const LikedSongs = () => {
     return (
-        <div className="content" style={{backgroundImage:"linear-gradient(#FA5077,#0F0F0F 30vh)", backgroundAttachment:"fixed"}}>
-            <DetailHead imgsrc={fav[0].image} heading="Playlist" desc={`by ${fav[0].singer}`} /> 
-            <SongListing props={fav[0].songsLst}/>
+        <div className="content" style={{backgroundImage:"linear-gradient(#422F81,#0F0F0F 30vh)", backgroundAttachment:"fixed"}}>
+            <DetailHead imgsrc="https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/0a17b4c50de9d543841e3742aa318abc774e10b2/img_db/like.jpg" heading="Playlist" desc={`Liked Songs`} /> 
+            <SongListing props={heartSOngs}/>
                
             
             
---
+++ src/db/Librarydb.js
@@ -0,0 +1,109 @@
+export const fav = [{
+    song: "Liggi",
+    singer: "Ritviz",
+    image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/Recently%20Played/ligi.jpg",
+
+    songsLst: [{
+        id: "1",
+        songName: "Liggi",
+        singerName: "Ritviz",
+        duration: "3:01"
+    }]
+}, {
+    song: "Unstoppable",
+    singer: "Dino James",
+    image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/MadeForYou/Unstoppable.jpg",
+    songsLst: [{
+        id: "1",
+        songName: "Unstoppable",
+        singerName: "Dino James",
+        duration: "4:39",
+    }]
+}, {
+    song: "Jab We Met",
+    singer: "Pritam, Sandesh Shandilya",
+    image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/Recently%20Played/met.jpg",
+    songsLst: [{
+        id: "1",
+        songName: "Rang Lageya",
+        singerName: "Mohit Chauhan",
+        duration: "3:47",
+    }]
+
+}, {
+    song: "Om Shanti Om",
+    singer: "Various Artist",
+    image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/Main%20agar%20kahoon.jpg",
+    songsLst: [{
+        id: "1",
+        songName: "Main Agar Kahoon",
+        singerName: "Sonu Nigam, Shreya Ghoshal",
+        duration: "5:08",
+    }, {
+        id: "2",
+        songName: "Aankhon Mein Teri",
+        singerName: "K.K.",
+        duration: "4:01",
+    }, {
+        id: "3",
+        songName: "Jag Soona Soona Lage",
+        singerName: "Rahat Fateh Ali Khan",
+        duration: "5:29",
+    }]
+}, {
+    song: "One Direction",
+    singer: "Various Artist",
+    image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone//main/img_db/Popular%20Band/oneDirection.jpg",
+    songsLst: [{
+        id: "1",
+        songName: "Barish",
+        singerName: "Darshan Raval",
+        duration: "2:51",
+    }]
+}]
+
+export const heartSOngs = [{
+        id: "1",
+        songName: "Kaise Mujhe",
+        singerName: "Shreya Ghoshal, Benny Dayal",
+        duration: "3:46",
+        image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/Guzarish.jpg",
+    }, {
+        id: "2",
+        songName: "Guzarish",
+        singerName: "Javed Ali,Sonu Nigam",
+        duration: "4:28",
+        image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/Guzarish.jpg",
+    }, {
+        id: "3",
+        songName: "Main Agar Kahoon",
+        singerName: "Sonu Nigam, Shreya Ghoshal",
+        duration: "5:08",
+        image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/Main%20agar%20kahoon.jpg",
+    }, {
+        id: "4",
+        songName: "Aankhon Mein Teri",
+        singerName: "K.K.",
+        duration: "4:01",
+        image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/Main%20agar%20kahoon.jpg",
+    }, {
+        id: "5",
+        songName: "Jag Soona Soona Lage",
+        singerName: "Rahat Fateh Ali Khan",
+        duration: "5:29",
+        image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/Main%20agar%20kahoon.jpg",
+    }, {
+        id: "6",
+        songName: "Bekhayali",
+        singerName: "Sachet Tandon",
+        duration: "6:12",
+        image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/kaise%20hua.jpg",
+    }, {
+        id: "7",
+        songName: "Tujhe Kitna Chane Laaga",
+        singerName: "Arijit Singh",
+        duration: "4:45",
+        image: "https://raw.githubusercontent.com/mimansha-swarup/spotify-ui-clone/main/img_db/you%20might%20like/kaise%20hua.jpg",
+    },
+
+]
