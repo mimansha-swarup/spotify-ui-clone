c033e4045476c064a7de994a961adde8f1c413c9
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-05-26 16:22:44 +0530

    refactor: seamntic tags

--- src/component/commonComponent/PlayBar.js
+++ src/component/commonComponent/PlayBar.js
@@ -4,26 +4,29 @@ import { AiOutlineMenuUnfold  } from 'react-icons/ai'
 import { IoVolumeLow  } from 'react-icons/io5'
 import { HiDeviceMobile  } from 'react-icons/hi'
 
+
 import SongController from './playbar_songController'
 import SongDetails from './PlayBar_SongDetails'
 
 
-const PlayBar = () => {
+const PlayBar = (props) => {
+    
     return ( 
         // <IconContext.Provider value={{ }}>
-        <div className="music-bar">
+        <footer className="music-bar">
             
             <SongDetails />
             <SongController />
             
             
+
             <div className="volume-controller row" >
                 <HiDeviceMobile className ="volume-controller-icons" />
                 <AiOutlineMenuUnfold className ="volume-controller-icons" />
                 <IoVolumeLow className ="volume-controller-icons" />
                 <hr />
             </div>
-        </div>
+        </footer>
       
     //   </IconContext.Provider>
      );
