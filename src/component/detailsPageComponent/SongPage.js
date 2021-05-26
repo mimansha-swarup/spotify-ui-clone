4ed088c2a921ce4eb058d8c6acd2876ead015a55
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-05-26 16:23:23 +0530

    color added

--- src/component/detailsPageComponent/SongPage.js
+++ src/component/detailsPageComponent/SongPage.js
@@ -1,6 +1,7 @@
 import "../Liked.css"
 import DetailHead from "./HeadBox"
 
+
 import SongListing from "./SongListing"
   
 
@@ -8,8 +9,9 @@ import SongListing from "./SongListing"
 const SongPage = (props) => {
     const data = props.location.state.check;
     
+    
     return (
-        <div className="content" style={{backgroundImage:"linear-gradient(#FA5077,#0F0F0F 30vh)", backgroundAttachment:"fixed"}}>
+        <div className="content" style={{backgroundImage:"linear-gradient(#1ED760,#0F0F0F 30vh)", backgroundAttachment:"fixed"}}>
             <DetailHead imgsrc={data.image} heading={data.song} desc={`by ${data.singer}`} /> 
             <SongListing props={data.songsLst}/>
             
